7bf154a5790d4e2fb9f7b01d803fe9c928d46ff9
iouhase <emilowithc@gmail.com>
2026-03-03 10:44:28 +0100

    stuff

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ impl CheckInCounter {
         Self { id, baggage_log }
     }
 
-    fn process_baggage(&self, baggage_id: &str) {
+    fn process_baggage(&self, baggage_id: &str, passenger_list: &Arc<Mutex<Vec<String>>>) {
+        let mut list = passenger_list.lock().unwrap();
         let mut log = self.baggage_log.lock().unwrap();
         log.push(format!(
             "Check-in {} registrerede bagage: {}",
@@ -26,7 +27,7 @@ fn main() {
     let baggage_log = Arc::new(Mutex::new(Vec::new()));
     let counter1 = CheckInCounter::new(1, baggage_log.clone());
     let handle = thread::spawn(move || {
-        counter1.process_baggage("BAG123");
+        counter1.process_baggage("BAG123", &passenger_list);
     });
     handle.join().unwrap();
 }

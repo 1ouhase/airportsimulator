dcd5620468210d58cc2929afd287f0e2d616263f
ZnowBaka <snorrebr@gmail.com>
2026-03-08 12:04:40 +0100

    okay we getting there

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,231 @@
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use std::thread;
+use chrono::Local;
+use std::io::{self, BufRead};
 
-struct CheckInCounter {
+struct Passenger {
+    number: u32,
+    flight: String,
+}
+
+struct Baggage {
+    id: String,
+    flight: String,
+}
+
+struct Sorter;
+
+impl Sorter {
+    fn start(belt: Arc<Mutex<Vec<Baggage>>>, sorted_belt: Arc<Mutex<Vec<Baggage>>>){
+        thread::spawn(move || {
+            loop {
+                let baggage = {
+                    let mut b = belt.lock().unwrap();
+                    b.pop()
+                };
+
+                match baggage {
+                    Some(bag) => {
+                        println!("[Sorter] sorting bag {} for flight {}", bag.id, bag.flight);
+                        thread::sleep(Duration::from_secs(1));
+                        println!("[Sorter] Bag {} sorted at {}", bag.id, Local::now().format("%H:%M%S"));
+                        sorted_belt.lock().unwrap().push(bag);
+                    }
+                    None => { thread::sleep(Duration::from_millis(500));}
+                }
+            }
+
+        });
+    }
+}
+
+struct Counter {
     id: u32,
-    baggage_log: Arc<Mutex<Vec<String>>>,
+    is_open: bool,
 }
 
-impl CheckInCounter {
-    fn new(id: u32, baggage_log: Arc<Mutex<Vec<String>>>) -> Self {
-        Self { id, baggage_log }
+impl Counter {
+    fn new(id: u32) -> Self{
+        Self { id, is_open: false}
     }
 
-    fn process_baggage(&self, baggage_id: &str) {
-        let mut log = self.baggage_log.lock().unwrap();
-
-        log.push(format!(
-            "Check-in {} registrerede bagage: {}",
-            self.id, baggage_id
-        ));
-
-        println!("Skranke {} registrerede bagage: {}", self.id, baggage_id);
+    fn open(&mut self) {
+        self.is_open = true;
+        println!("[System] counter {} is open", self.id);
     }
-}
-struct PassengerSummoner {
-    id: u32,
-    passenger_list: Arc<Mutex<Vec<String>>>,
-}
-
-impl PassengerSummoner {
-    fn new(id: u32, passenger_list:Arc<Mutex<Vec<String>>>) -> Self {
-        Self { id, passenger_list}
+    
+    fn close(&mut self) {
+        self.is_open = false;
+        println!("[System] counter {} is now closed", self.id);
     }
-    fn start_summon(&self){
-        let passenger_list = Arc::clone(&self.passenger_list);
-        let summoner_id = self.id;
+
+    fn start(id: u32,
+        queue: Arc<Mutex<Vec<Passenger>>>, 
+        belt: Arc<Mutex<Vec<Baggage>>>,
+        counter: Arc<Mutex<Vec<Counter>>>,
+        bag_counter: Arc<Mutex<u32>>) {
 
         thread::spawn(move || {
-            let mut passenger_counter = 0;
             loop {
-                thread::sleep(Duration::from_secs(2));
-                
-                passenger_counter += 1;
-                let passenger_id = format!("PASS-{}-{}", summoner_id, passenger_counter);
-                
-                let mut list = passenger_list.lock().unwrap();
-                list.push(passenger_id.clone());
-                println!("Summoner {}: Created passenger {}", summoner_id, passenger_id);
+                let is_open = {
+                    let counters = counters.lock().unwrap();
+                    counters.iter().find(|c| c.id == id).unwrap().is_open
+                };
+
+                if is_open {
+                    let passenger = {
+                        let mut q = queue.lock().unwrap();
+                        q.pop()
+                    };
+                    match passenger {
+                        Some(p) => {
+                            println!("[Counter {}] Checking in passenger {} for flight {}", id, p.number, p.flight);
+                            thread::sleep(Duration::from_secs(3));
+
+                            let bag_id = {
+                                let mut bc = bag_counter.lock().unwrap();
+                                *bc += 1;
+                                format!("BAG-{:03}", bc)
+                            };
+                            let baggage = Baggage { id: bag_id.clone(), flight: p.flight.clone() };
+                            belt.lock().unwrap().push(baggage);
+                            println!("[Counter {}] Bag {} for flight {} placed on belt", id, bag_id, p.flight);
+                        }
+                        None => { thread::sleep(Duration::from_millis(500));}
+                    }
+                } else {
+                    thread::sleep(Duration::from_millis(500));
+                }
+            }
+        });
+    }
+}
+
+struct Gate {
+    id: u32,
+    flight: String,
+    is_open: bool,
+}
+
+impl Gate {
+    fn new(id: u32, flight: &str) -> Self {
+        Self { id, flight: String::from(flight), is_open: false }
+    }
+
+    fn open(&mut self) {
+        self.is_open = true;
+        println!("[System] Gate {} is now open", self.id);
+    }
+
+    fn close(&mut self) {
+        self.is_open = false;
+        println!("[System] Gate {} is now closed", self.id);
+    }
+
+    fn start(id: u32, flight: String, sorted_belt: Arc<Mutex<Vec<Baggage>>>, gates: Arc<Mutex<Vec<Gate>>>) {
+        thread::spawn(move || {
+            loop {
+                let is_open = {
+                    let gates = gates.lock().unwrap();
+                    gates.iter().find(|g| g.id == id).unwrap().is_open
+                };
+
+                if is_open {
+                    let bag = {
+                        let mut belt = sorted_belt.lock().unwrap();
+                        let pos = belt.iter().position(|b| b.flight == flight);
+                        pos.map(|i| belt.remove(i))
+                    };
+
+                    match bag {
+                        Some(b) => {
+                            println!("[Gate {}] Received bag {} for flight {} at {}", id, b.id, b.flight, Local::now().format("%H:%M:%S"));
+                        }
+                        None => { thread::sleep(Duration::from_millis(500)); }
+                    }
+                } else {
+                    thread::sleep(Duration::from_millis(500));
+                }
             }
         });
     }
 }
 
 fn main() {
-    let passenger_list: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
-    let baggage_log = Arc::new(Mutex::new(Vec::new()));
+    let flight = ["SAS001", "SAS002", "SAS003"];
+    let queue: Arc<Mutex<Vec<Passenger>>> = Arc::new(Mutex::new(Vec::new()));
+    let belt: Arc<Mutex<Vec<Baggage>>> = Arc::new(Mutex::new(Vec::new()));
+    let sorted_belt: Arc<Mutex<Vec<Baggage>>> = Arc::new(Mutex::new(Vec::new()));
 
-    let summoner1 = PassengerSummoner::new(1, passenger_list.clone());
-    let counter1 = CheckInCounter::new(1, baggage_log.clone());
-    summoner1.start_summon();
-    let handle = thread::spawn(move || {
-        counter1.process_baggage("BAG123");
+    let gates: Arc<Mutex<Vec<Gate>>> = Arc::new(Mutex::new(vec![
+            Gate::new(1, "SAS001"),
+            Gate::new(2, "SAS002"),
+            Gate::new(3, "SAS003"),
+    ]));
+
+    let counter: Arc<Mutex<Vec<Counter>>> = Arc::new(Mutex::new(vec![
+            Counter::new(1),
+            Counter::new(2),
+    ]));
+
+    let bag_counter = Arc::new(Mutex::new(0u32));
+
+    let queue_for_sum = Arc::clone(&queue);
+    thread::spawn(move || {
+        let mut count = 0;
+        loop {
+            thread::sleep(Duration::from_secs(3));
+            count += 1;
+            let flight = flight[count as usize % flight.len()];
+            let passenger = Passenger { number: count, flight: String::from(flight) };
+            let mut q = queue_for_sum.lock().unwrap();
+            println!("[Summoner] passenger {} arrived for flight {}", passenger.number, passenger.flight);
+            q.push(passenger);
+        }
     });
-    handle.join().unwrap();
+
+    for counter_id in 1..=2 {
+        Counter::start(
+            counter_id,
+            Arc::clone(&queue),
+            Arc::clone(&belt),
+            Arc::clone(&counters), 
+            Arc::clone(&bag_counter),
+            );
+    }
+
+    Sorter::start(Arc::clone(&belt), Arc::clone(&sorted_belt));
+
+    let gate_flights = vec![
+        (1, "SAS001"), (2, "SAS002"), (3, "SAS003")
+    ];
+    for (gate_id, flight) in gate_flights {
+        Gate::start(
+            gate_id,
+            String::from(flight),
+            Arc::clone(&sorted_belt),
+            Arc::clone(&gates),
+        );
+    }
+
+    // user input to open start threads go here
+    //
+    //
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
